1b47bbda6914985795e49567cd92653961c7cb06
mishka8 <144250192+mishka8@users.noreply.github.com>
2024-12-14 17:58:04 +0700

    Update Haffman.cpp

--- Haffman.cpp
+++ Haffman.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -7,7 +10,7 @@ struct Node//узел дерева
 {
     char key;//символ
     unsigned long long period_key;//частота символа
-    
+
     Node *left;//левый потомок
     Node *right;//правый потомок
 };
@@ -15,12 +18,12 @@ struct Node//узел дерева
 Node* getNode(char key, unsigned long long period_key, Node *left, Node *right)//выделение нового узла дерева
 {
     Node* node = new Node();
-    
+
     node->key = key;
     node->period_key = period_key;
     node->left = left;
     node->right = right;
-    
+
     return node;
 }
 
@@ -32,12 +35,29 @@ struct compare//сравнение для создания приоритета
     }
 };
 
-void tableOfValueCodes(Node *root, string str, )
 
+//вспомогательная функция для проверки?????
+bool lastNode(Node *main)
+{
+    return main->left == nullptr && main->right == nullptr;
+}
 
+void TableHaffmanCodes(Node *main, string str, unordered_map<char, string> &HaffmanCodes)
+{
+    if(main == nullptr)
+        return;
+
+    if(!lastNode(main))
+    {
+        HaffmanCodes[main->key] = str;
+    }
+
+    TableHaffmanCodes(main->left, str + "0", HaffmanCodes);
+    TableHaffmanCodes(main->right, str + "1", HaffmanCodes);
+}
 
 int main()
 {
-  
+    cout << "Hello World!" << endl;
     return 0;
 }
